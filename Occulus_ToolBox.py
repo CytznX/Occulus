511d302a80be830efb29c7d790f9b99d04cadaff
Max Seifert <seiferm1@my.erau.edu>
2014-07-15 14:55:30 -0400

    "Added ability to capture and record video "
    Because thats what this pakage was suppose to do muhahaha...

---
+++ Occulus_ToolBox.py
@@ -0,0 +1,235 @@
+"""
+pyClass I use for testing and recording some image analysis filters
+
+Created By: Citizenx
+"""
+
+#Some 
+import time
+import os
+import numpy as np
+import cv2
+import cv
+
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+from tkFileDialog import askdirectory
+
+class OculusMaximus():
+
+	def __init__(self, showOutput = True, haarDetectInterval = 2):
+
+		#Capture Some Initialization
+		self._showOutput = showOutput
+		self._haarDetectInterval = haarDetectInterval
+
+		#Some Default Resoure Folder Stuff
+		self.Videos_Root_Folder = "vid/"
+		self.Images_Root_Folder = "img/"
+		self.Resources_Root_Folder = "res/"
+
+		#Check to see if record directorys exists
+		for checkDirectory in [ self.Videos_Root_Folder, self.Images_Root_Folder, self.Resources_Root_Folder]:
+
+			if not os.path.isdir(checkDirectory):
+				os.makedirs(checkDirectory)
+				print "No "+checkDirectory+" Directory... EmptyOne Created"
+
+		#Sets some initial Flag Variables
+		self._haarDetect_Flag = False
+		self._haarMulti_Flag = False
+		self._Run_Flag = True
+		self._RecordVideo = False
+		
+		Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+		filename =  askopenfilename(initialdir = self.Resources_Root_Folder+"haarFilter_Face_Basic/")# show an "Open" dialog box and return the path to the selected file
+		
+		#default haar Rec Directory
+		if os.path.isfile(filename):
+
+			self._haarRecFilterFlag = (True, None)
+			self._haarRecogFilter = filename
+		
+		elif os.path.isfile(self.Resources_Root_Folder+"haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"):
+			print "WOOPC Something woonkie happened with your haar filter selection...\nhad to default to: "+self.Resources_Root_Folder+"haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"
+			self._haarRecFilterFlag = (True, None)
+			self._haarRecogFilter = self.Resources_Root_Folder+"haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"
+		
+		else :
+			self._haarRecFilterFlag = (False, "File Doesnt Exist")
+
+		#Pull in the cascade feature template we want to use
+		try:
+			self.cascade = cv2.CascadeClassifier(self._haarRecogFilter)
+		except Exception, e:
+			self._haarRecFilterFlag = (False, e)
+
+
+		#Create the Video Interfaces
+		self._MainCaptureIterFace = cv2.VideoCapture(0)
+		
+
+	def haarDetect(self, img):
+	    rects = self.cascade.detectMultiScale(img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+
+	    if len(rects) == 0:
+	        return [], img
+	    rects[:, 2:] += rects[:, :2]
+	    return rects, img
+
+	#Draw Rectangles on a image
+	def box(self, rects, img):
+		for x1, y1, x2, y2 in rects:
+			cv2.rectangle(img, (x1, y1), (x2, y2), (127, 255, 0), 2)
+	
+
+	#NOT DONE YET
+	def compare(self, rects, img):
+		crop_img = img[y1:y2, x1:x2, :] 
+		
+	def run(self):
+
+		#Check If i want output shown
+		if self._showOutput:
+			cv2.namedWindow("OutPut") #Create The Output Window
+
+		#Initializ some counter variables 
+		_haarCounter = 0
+		_vidcount = 0
+		_imgcount = 0
+
+		#Variable used to store open Output video file reference
+		_videoOut = None
+
+		while(self._Run_Flag):
+
+			_,orig_img = self._MainCaptureIterFace.read()
+			orig_img = cv2.flip(orig_img, 1)
+			output_img = orig_img.copy()
+
+			if (self._haarDetect_Flag == True):
+
+				orig_img = cv2.GaussianBlur(orig_img, (5,5), 0)
+			
+				if _haarCounter%self._haarDetectInterval==0:
+					rects, img = self.haarDetect(orig_img)
+					
+				self.box(rects, output_img)
+				_haarCounter+=1
+
+			if self._RecordVideo:
+				_videoOut.write(output_img)
+
+
+			if self._showOutput:
+				cv2.imshow("OutPut",output_img)
+
+			#I Never rember what wait key does 
+			#But one of the best things is that you can pull keypresses
+			x = cv2.waitKey(1)
+
+			#If you press q baill out
+			if x & 0xFF == ord('q'):
+				cv2.destroyAllWindows()
+				self._MainCaptureIterFace.release()
+				self._Run_Flag = False
+
+				if self._RecordVideo:
+					_videoOut.release()
+
+				print "Bailing Out... Cya!!!"
+
+			#IF F is pressed do some basic Haar recognition
+			elif x & 0xFF == ord('h'):
+				if self._haarRecFilterFlag[0]:
+					self._haarDetect_Flag = not self._haarDetect_Flag
+
+					#value_when_true if condition else value_when_false
+					print "Haar Filter Tracking " + ("on" if self._haarDetect_Flag else "off")
+				else:
+					print "Nope Sry... Something went wrong with Resource: "+self._haarRecogFilter, self._haarRecFilterFlag[1]
+
+			elif x & 0xFF == ord('n'):
+
+				#Ask for new file
+				filename =  askopenfilename(initialdir = self.Resources_Root_Folder+"haarFilter_Face_Basic/")# show an "Open" dialog box and return the path to the selected file
+		
+				#Make sure file exists
+				if os.path.isfile(filename):
+
+					self._haarRecFilterFlag = (True, None)
+					self._haarRecogFilter = filename
+				
+				elif os.path.isfile(self.Resources_Root_Folder+"haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"):
+					print "WOOPC Something woonkie happened with your haar filter selection...\nhad to default to: "+self.Resources_Root_Folder+"haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"
+					self._haarRecFilterFlag = (True, None)
+					self._haarRecogFilter = self.Resources_Root_Folder+"haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"
+				
+				else :
+					self._haarRecFilterFlag = (False, "File Doesnt Exist")
+
+				#Pull in the cascade feature template we want to use
+				try:
+					self.cascade = cv2.CascadeClassifier(self._haarRecogFilter)
+				except Exception, e:
+					self._haarRecFilterFlag = (False, e)
+
+			#If M is pressed use multi haarfilters in selected dir
+			elif x & 0xFF == ord('m'):
+				if self._haarMulti_Flag[0] == False:
+
+					dirname = askdirectory(parent=root, initialdir='/home/', title='Select your pictures folder')
+					print "The Directory name: ", dirname
+
+					if os.path.isdir(dirname):
+						self._haarMulti_Flag = (True, dirname)
+					else:
+						self._haarMulti_Flag = (False, None)
+				else:
+					self._haarMulti_Flag = (False, None)
+
+
+			#If Enter Is pressed Enter sart video Record
+			elif x & 0xFF == ord('c'):
+				if not self._RecordVideo:
+					_vidcount+=1
+					codec = cv.CV_FOURCC('D','I','V','X')
+
+					size = (int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
+							int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
+
+					_videoOut = cv2.VideoWriter()
+					_videoOut.open(self.Videos_Root_Folder+'OutVideo('+str(_vidcount)+').avi', codec, 30, size, 1)
+					
+					self._RecordVideo = True
+					print "Sarted Video record"
+
+				else:
+					#Close video out File And set capture boolean to false
+					_videoOut.release()
+					self._RecordVideo = False
+
+					print "Ended Video record"
+
+			elif x == 32:
+				#TSH SPACE: 1048608
+				print "Catured Images", self.Images_Root_Folder+"SnapShop("+str(_imgcount)+").jpg"
+				
+				#Capture Image
+				cv2.imwrite(self.Images_Root_Folder+"SnapShop("+str(_imgcount)+").jpg", output_img)
+				
+				#Inc counter Variable
+				_imgcount+=1
+
+			#case you want to know what key you pressed
+			elif x != -1:
+				print "Key Press ==",x
+	
+	#How to make a propertry 
+	#-------------------------------------- VVV RIGHT HERE!!! VVV
+	#<property_name> = property(<getter_Method_Name>, <setter_Method_Name>)
+
+if __name__ == '__main__':
+	runner = OculusMaximus()
+	runner.run()
+
