df518244c7b2464a44a143bb819bc969b902ebb8
CitizenX <mseifert182@gmail.com>
2014-07-23 00:47:20 -0400

    "Delinted Everything"
    Just added a py dilinter to my ide... now code adhears to most of PEP8...
    Makes code more uniform

--- Occulus_Portal.py
+++ Occulus_Portal.py
@@ -4,23 +4,23 @@ pyClass I use for testing and recording some image analysis filters
 Created By: Citizenx
 """
 
-#Some 
-import time, datetime
+#Some Basic imports
+import datetime
 import os
-import numpy as np
-import cv2, cv
+import cv2
+import cv
 
-import threading
-
+#Some Renamed Import
 from Tkinter import Tk
 from tkFileDialog import askopenfilename
 from tkFileDialog import askdirectory
 
 import Occulus_Threader
 
+
 class OculusMaximus():
 
-	def __init__(self, showOutput = True, haarDetectInterval = 2):
+	def __init__(self, showOutput=True, haarDetectInterval=2):
 
 		#Capture Some Initialization
 		self._showOutput = showOutput
@@ -35,7 +35,7 @@ class OculusMaximus():
 		self._CurrentHaarXML = self.Resources_Root_Folder+"haarFilter_Face_Basic/"
 
 		#Check to see if record directorys exists
-		for checkDirectory in [ self.Videos_Root_Folder, self.Images_Root_Folder, self.Resources_Root_Folder]:
+		for checkDirectory in [self.Videos_Root_Folder, self.Images_Root_Folder, self.Resources_Root_Folder]:
 			if not os.path.isdir(checkDirectory):
 				os.makedirs(checkDirectory)
 				print "No "+checkDirectory+" Directory... EmptyOne Created"
@@ -51,12 +51,11 @@ class OculusMaximus():
 		self._ThreadOverLoard.start()
 
 		#Heres how i create those file viewer dialog boxes
-		Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-		_filename =  askopenfilename(initialdir = self._CurrentHaarXML, title='Select Your Initial Haar Filter')# show an "Open" dialog box and return the path to the selected file
-
+		Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+		_filename = askopenfilename(initialdir=self._CurrentHaarXML, title='Select Your Initial Haar Filter')  # show an "Open" dialog box and return the path to the selected file
 
 		self._XML_isValid = self._ThreadOverLoard.newFilter(_filename)
-		
+
 		if self._XML_isValid[0]:
 			self._CurrentHaarXML = _filename
 		else:
@@ -69,29 +68,28 @@ class OculusMaximus():
 		self._LastImageCapture = datetime.datetime.now()
 
 	#Draw Rectangles on a image
-	def box(self, rects, img, color = (127, 255, 0)):
+	def box(self, rects, img, color=(127, 255, 0)):
 		for x1, y1, x2, y2 in rects:
 			cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
-	
+
 	def run(self):
 
 		#Check If i want output shown
 		if self._showOutput:
 			cv2.namedWindow("OutPut "+str(int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)))+" "
-							+str(int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))) #Create The Output Window
+							+ str(int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))))
 
-		#Initializ some counter variables 
+		#Initializ some counter variables
 		_vidcount = 0
 		_imgcount = 0
 
 		#Variable used to store open Output video file reference
 		_videoOut = None
 
-
 		while(self._Run_Flag):
 
 			#Capture New Frame and record when you did it
-			_,orig_img = self._MainCaptureIterFace.read()
+			_, orig_img = self._MainCaptureIterFace.read()
 
 			#Comput Fps
 			_currentCaptureTime = datetime.datetime.now()
@@ -101,13 +99,12 @@ class OculusMaximus():
 			#Flip the image so its mirrored and make a copy to be used later
 			orig_img = cv2.flip(orig_img, 1)
 			output_img = orig_img.copy()
-			
+
 			if self._haarDetect_Flag:
 
 				#I pass original because the overloard makes an internal copy that it passes off to its subthreads
 				self._ThreadOverLoard.addFrame4Haar(orig_img)
 				self.box(self._ThreadOverLoard.getLatestHaarAnalysis(), output_img)
-
 
 			#Write Video out to selected file
 			if self._RecordVideo:
@@ -116,18 +113,18 @@ class OculusMaximus():
 			#Display image
 			if self._showOutput:
 
-				_DispText = "FPS: %02d"%(currentFPS)
+				_DispText = "FPS: %02d" % (currentFPS)
 
-				if self._haarDetect_Flag: 
+				if self._haarDetect_Flag:
 					Temp = self._ThreadOverLoard.getInfo()
-					_DispText += " #Threads: %d SleepTime: %.2f" % Temp 
+					_DispText += " #Threads: %d SleepTime: %.2f" % Temp
 
-				cv2.putText(output_img, _DispText,(5,int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))-10),cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0,0,0))
+				cv2.putText(output_img, _DispText, (5, int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 0))
 
 				cv2.imshow("OutPut "+str(int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)))+" "
-							+str(int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))) ,output_img)
+							+ str(int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))), output_img)
 
-			#I Never rember what wait key does 
+			#I Never rember what wait key does
 			#But one of the best things is that you can pull keypresses
 			x = cv2.waitKey(1)
 
@@ -141,13 +138,15 @@ class OculusMaximus():
 				if self._RecordVideo:
 					_videoOut.release()
 
+				#Signals Main Vid Thread is done... and where waiting on subthreads
 				print "Bailing Out... Cya!!!"
+
 				#-----------------------------------------
 				#Make sure to join all threads Bellow
 				#-----------------------------------------
+				self._ThreadOverLoard.join()  # Wait for the Overloard to close out clean
 
-				self._ThreadOverLoard.join() #Wait for the Overloard to close out clean
-
+				#One Last Final Goodbye
 				print "Le fin"
 
 			#IF F is pressed do some basic Haar recognition
@@ -160,14 +159,17 @@ class OculusMaximus():
 				else:
 					print "---Nope Sry... Something went wrong with XML Resource---\n", self._XML_isValid
 
+			#If N is Pressed... Load in new XML file
 			elif x & 0xFF == ord('n'):
-				#Get New Filter
+
 				#Ask for new file
-				_filename =  askopenfilename(initialdir = self.Resources_Root_Folder+"haarFilter_Face_Basic/", title='Select Your Haar Filter')# show an "Open" dialog box and return the path to the selected file
+				_filename = askopenfilename(initialdir=self.Resources_Root_Folder+"haarFilter_Face_Basic/", title='Select Your Haar Filter')
 				print "new file requested", _filename
 
+				#Checks if were valid
 				self._XML_isValid = self._ThreadOverLoard.newFilter(_filename)
 
+				#If where valid... Bailout
 				if self._XML_isValid[0]:
 					self._CurrentHaarXML = _filename
 				else:
@@ -178,20 +180,18 @@ class OculusMaximus():
 				dirname = askdirectory(initialdir=self.Resources_Root_Folder, title='Select A Directory Folder')
 				print "The Directory name: ", dirname
 
-
-
 			#If Enter Is pressed Enter sart video Record
 			elif x & 0xFF == ord('c'):
 				if not self._RecordVideo:
-					_vidcount+=1
-					codec = cv.CV_FOURCC('D','I','V','X')
+					_vidcount += 1
+					codec = cv.CV_FOURCC('D', 'I', 'V', 'X')
 
 					size = (int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)),
 							int(self._MainCaptureIterFace.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)))
 
 					_videoOut = cv2.VideoWriter()
 					_videoOut.open(self.Videos_Root_Folder+'OutVideo('+str(_vidcount)+').avi', codec, 30, size, 1)
-					
+
 					self._RecordVideo = True
 					print "Sarted Video record"
 
@@ -205,23 +205,22 @@ class OculusMaximus():
 			elif x == 32:
 				#TSH SPACE: 1048608
 				print "Catured Images", self.Images_Root_Folder+"SnapShop("+str(_imgcount)+").jpg"
-				
+
 				#Capture Image
 				cv2.imwrite(self.Images_Root_Folder+"SnapShop("+str(_imgcount)+").jpg", output_img)
-				
+
 				#Inc counter Variableh
-				_imgcount+=1
+				_imgcount += 1
 
 			#case you want to know what key you pressed
 			elif x != -1:
-				print "Key Press ==",x
-	
+				print "Key Press ==", x
 
-	#How to make a propertry 
+
+	#How to make a propertry
 	#-------------------------------------- VVV RIGHT HERE!!! VVV
 	#<property_name> = property(<getter_Method_Name>, <setter_Method_Name>)
 
 if __name__ == '__main__':
 	runner = OculusMaximus()
 	runner.run()
-
--- Occulus_Threader.py
+++ Occulus_Threader.py
@@ -9,21 +9,26 @@ Python 2.7
 #Importing Necisary moduals
 import os
 import threading
-import time, datetime
-import numpy as np
-import cv2, cv
+import time
+import cv2
 
+#Not needed yet but might
+import datetime
+import numpy as np
+
+#Grab a que to better handle exclusive access
 from Queue import Queue
+
 
 #Thread class that watches Everything... Litterally
 class OcculusOverloard(threading.Thread):
 
 	#Deh constructor... Muhahaha
-	def __init__(self, haarDetectInterval=1, ResRoot = "res/", DefaultFilter = "haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"):
-		
+	def __init__(self, haarDetectInterval=1, ResRoot="res/", DefaultFilter="haarFilter_Face_Basic/haarcascade_frontalface_alt.xml"):
+
 		#Innitials As Thread
 		threading.Thread.__init__(self)
-		
+
 		#Store Some Innit Variables
 		self._haarDetectInterval = haarDetectInterval
 		self.Resources_Root_Folder = ResRoot
@@ -39,8 +44,8 @@ class OcculusOverloard(threading.Thread):
 		self._CurrentRunningThreads = []
 
 		#Concurency and Effiecientcy stuff for haar detection threads
-		self._CurrentHaarRecsLock = threading.Lock()#IdK if i should declare this variable as "hidden" or not... =/
-		self._haarCurrentRects = ([],0)
+		self._CurrentHaarRecsLock = threading.Lock()  # IdK if i should declare this variable as "hidden" or not... =/
+		self._haarCurrentRects = ([], 0)
 		self._haarCounter = 0
 
 		#The Filter Flag for determine har filter validity and the current haarXML file
@@ -49,7 +54,7 @@ class OcculusOverloard(threading.Thread):
 
 		#Thread Printer Managment
 		self._ToBePrinted = Queue()
-		self._PrintLock = threading.Lock() #Dont think ill need this... but will see
+		self._PrintLock = threading.Lock()  # Dont think ill need this... but will see
 
 	def newFilter(self, filename):
 
@@ -58,15 +63,15 @@ class OcculusOverloard(threading.Thread):
 
 			self._haarRecFilterFlag = (True, filename)
 			self._CurrentHaarXML = filename
-		
+
 		#else notify user and my use default file
 		elif os.path.isfile(self._DefaultHaarFilter):
-			self._ToBePrinted.put( "WOOPC Something woonkie happened with your haar filter selection...\nhad to default to: "+self._DefaultHaarFilter)
+			self._ToBePrinted.put("WOOPC Something woonkie happened with your haar filter selection...\nhad to default to: "+self._DefaultHaarFilter)
 			self._haarRecFilterFlag = (True, self._DefaultHaarFilter)
-		
+
 		#else notify user and my use default file
-		else :
-			self._ToBePrinted.put( "UH-OH... Something realy bad has happend, no default haar filter either")
+		else:
+			self._ToBePrinted.put("UH-OH... Something realy bad has happend, no default haar filter either")
 			self._haarRecFilterFlag = (False, None)
 			self._CurrentHaarXML = None
 
@@ -84,41 +89,46 @@ class OcculusOverloard(threading.Thread):
 		if self._haarRecFilterFlag[0]:
 
 			#Little salt and pepper i added for low power machines
-			if self._haarCounter%self._haarDetectInterval==0:
+			if self._haarCounter % self._haarDetectInterval == 0:
 
 				#Create New Detection Thread
-				threading.Thread(target=self._DetectionSubThread, args = (img.copy(), self._haarCounter)).start() #Start the Thread....
+				threading.Thread(target=self._DetectionSubThread, args = (img.copy(), self._haarCounter)).start()  # Start the Thread....
 
 			#Inc the Haar counter
-			self._haarCounter +=1
+			self._haarCounter += 1
 
 	def getInfo(self):
 		return threading.active_count(), self._sleepTimeMain
 
+	#Used To Get Total Analysis
 	def getLatestHaarAnalysis(self):
-		
+
+		#Creates a storage container
 		returnRecs = []
+
+		#Grab Key
 		self._CurrentHaarRecsLock.acquire()
 
+		#Throw latest in greatest... =)
 		try:
 			returnRecs = self._haarCurrentRects[0]
 
 		finally:
 			self._CurrentHaarRecsLock.release()
 
+		#return the container
 		return returnRecs
 
 	def getCurrentHaarXML(self):
 		return self._haarRecFilterFlag[1]
 
-
 	def _DetectionSubThread(self, passed_img, haarCount):
 
 		#Perform a simple blur before Haar Analysis
-		passed_img = cv2.GaussianBlur(passed_img, (5,5), 0)
-	
+		passed_img = cv2.GaussianBlur(passed_img, (5, 5), 0)
+
 		#Perform Haar Analysis
-		rects = self.cascade.detectMultiScale(passed_img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20,20))
+		rects = self.cascade.detectMultiScale(passed_img, 1.3, 4, cv2.cv.CV_HAAR_SCALE_IMAGE, (20, 20))
 
 		#If the list isnt empty do some formating and add it to master
 		if not len(rects) == 0:
@@ -127,7 +137,7 @@ class OcculusOverloard(threading.Thread):
 		self._CurrentHaarRecsLock.acquire()
 
 		try:
-			if(haarCount>self._haarCurrentRects[1]):
+			if(haarCount > self._haarCurrentRects[1]):
 				self._haarCurrentRects = (rects, haarCount)
 
 			else:
@@ -141,23 +151,19 @@ class OcculusOverloard(threading.Thread):
 
 	#yuuuuuuuuuuuuuuuuuuup.... the runner...
 	def run(self):
-		
+
 		#Stay operational so long as the program is running
 		while self._Run_Flag:
 
 						#If There is something to be Printed Lock the array
 			if self._ToBePrinted.empty():
-				self._sleepTimeMain+=0.01
+				self._sleepTimeMain += 0.01
 			else:
 				if self._sleepTimeMain >= 0.01:
-					self._sleepTimeMain-=0.01
+					self._sleepTimeMain -= 0.01
 
 				#Print the first element of array
 				print self._ToBePrinted.get()
 
 			#else Sleep the Thread and wait....
 			time.sleep(self._sleepTimeMain)
-
-			
-
-	
